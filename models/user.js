9a0cdf87627708707c44b5fe2da250cef307151c
Annytitinha <anaiana.comex@gmail.com>
2020-03-12 10:52:41 +0000

    fix

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require( 'mongoose'); 
+var userSchema = new mongoose. Schema({ 
+  email: { type: String, unique: true, lowercase: password: String, username: String, gender: { 
+  type: String, enum: ['MALE", 'FEMALE'] 
+  phone: Number 
+);
+
+module. exports = mongoose.model('User', userSchema);
